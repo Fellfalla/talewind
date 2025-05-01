fcf43f23ec7396d83a1d640153aec9d85e8e7685
Markus Weber <markus.weberino@gmail.com>
2025-05-01 23:15:47 +0000

    fix inventory issues

--- src/talewind/mcp_servers/inventory/server.py
+++ src/talewind/mcp_servers/inventory/server.py
@@ -1,6 +1,8 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp import Context
 
 # aliases
+# TODO: use struct for item containing identifier and desciption/properties separately.
 ItemT = str
 OwnerT = str
 InventoryID = OwnerT
@@ -12,13 +14,32 @@ class Inventory:
         assert owner != "", "Owner must be a non-empty string"
 
         self.items: list[ItemT] = []
-        self.owner: OwnerT = ""
+        self.owner: OwnerT = owner
+
+    @property
+    def identifier(self) -> InventoryID:
+        return self.owner
 
 
 # stick to simple list as number of inventories are not expected to be large
-_inventories: list[Inventory] = []
+_inventories: dict[Inventory] = []
 
 mcp = FastMCP("inventory")
+
+
+def get_inventory(inventory_id: InventoryID) -> Inventory:
+    """
+    Get the inventory for the given owner.
+
+    Args:
+        inventory_id (str): The owner of the inventory. Must be a non-empty string.
+
+    Returns:
+        Inventory: The inventory for the given owner.
+    """
+    for inventory in _inventories:
+        if inventory.identifier == inventory_id:
+            return inventory
 
 
 @mcp.tool(name="create_inventory")
@@ -29,7 +50,8 @@ async def create_inventory(owner: OwnerT) -> str:
     Args:
         owner (str): The owner of the inventory. Must be a non-empty string.
     """
-    if owner in _inventories:
+    global _inventories
+    if get_inventory(owner) is not None:
         return f"Inventory for {owner} already exists."
 
     try:
@@ -41,26 +63,93 @@ async def create_inventory(owner: OwnerT) -> str:
 
 
 @mcp.resource("inventory://")
+@mcp.tool("list_inventories")
 async def list_inventories() -> list[InventoryID]:
     """
     List all available inventories.
     """
-    return list(map(lambda x: x.owner, _inventories))
+    return list(map(lambda x: x.identifier, _inventories))
 
 
-@mcp.resource("inventory://{owner}/items")
-async def list_items(owner: OwnerT) -> list[InventoryID]:
+# NOTE (@weber): if the arg name does not match the placeholder in resource, the mcp server will timeout during bootup
+@mcp.resource("inventory://{inventory_id}/items")
+@mcp.tool("list_items")
+async def list_items(inventory_id: InventoryID) -> list[ItemT]:
     """
     List all items in the inventory.
 
     Args:
-        owner (str): The owner of the inventory. Must be a non-empty string.
+        inventory_id (str): The owner of the inventory. Must be a non-empty string.
 
     """
-    if owner not in _inventories:
-        return f"Inventory for {owner} does not exist."
+    inventory = get_inventory(inventory_id)
+    if inventory is None:
+        return f"Inventory for {inventory_id} does not exist."
 
-    return _inventories[owner].items
+    return inventory.items
+
+
+@mcp.tool(name="add_item")
+async def add_item(inventory_id: InventoryID, item: ItemT, ctx: Context) -> str:
+    """
+    Add an item to the inventory.
+
+    Args:
+        inventory_id (str): The owner of the inventory. Must be a non-empty string.
+        item (str): The item to add. Must be a non-empty string.
+    """
+    global _inventories
+    inventory = get_inventory(inventory_id)
+    if inventory is None:
+        ctx.error(f"Inventory for {inventory_id} does not exist.")
+        return f"Inventory for {inventory_id} does not exist."
+
+    # NOTE (@weber): Allow duplicate items
+    inventory.items.append(item)
+    return f"Item {item} added to inventory for {inventory_id}."
+
+
+@mcp.tool(name="remove_item")
+async def remove_item(inventory_id: InventoryID, item: ItemT) -> str:
+    """
+    Remove an item from the inventory.
+
+    Args:
+        inventory (str): The owner of the inventory. Must be a non-empty string.
+        item (str): The item to remove. Must be a non-empty string.
+    """
+    global _inventories
+    inventory = get_inventory(inventory_id)
+    if inventory is None:
+        return f"Inventory for {inventory_id} does not exist."
+
+    if item not in inventory.items:
+        return f"Item {item} not found in inventory for {inventory_id}."
+
+    return f"Item {item} removed from inventory for {inventory_id}."
+
+
+@mcp.tool(name="update_item")
+async def update_item(inventory_id: InventoryID, item: ItemT, ctx: Context) -> str:
+    """
+    Update an item in the inventory.
+
+    Args:
+        inventory (str): The owner of the inventory. Must be a non-empty string.
+        item (str): The item to update. Must be a non-empty string.
+    """
+    global _inventories
+    inventory = get_inventory(inventory_id)
+    if inventory is None:
+        return f"Inventory for {inventory_id} does not exist."
+
+    if item not in inventory.items:
+        return f"Item {item} not found in inventory for {inventory_id}."
+
+    # NOTE (@weber): This doesnt make any sense until we separate item identifier and description from each other
+    inventory.items[inventory.items.index(item)] = item
+
+    return f"Item {item} updated in inventory for {inventory_id}."
 
 
 async def serve():
